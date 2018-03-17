ef4a9016d0f714c7fbb6b69f09c2b412445ccb99
Raphael Amorim <rapha850@gmail.com>
2018-03-17 17:55:09 -0300

    1 step

--- algorithm-abstraction.js
+++ algorithm-abstraction.js
@@ -1,40 +1,55 @@
+console.time('ended with')
+
 var input1 = {
   color: 1,
-  background: 2,
+  background: 4,
   fontSize: 3
 }
 
 var input2 = {
-  color: 2,
+  color: 1,
   background: 2
 }
 
 var input3 = {
-  color: 1,
-  fontSize: 3
+  color: 2,
+  fontSize: 5
 }
 
 const rules = [];
+const transformedRules = {};
 
-function normalize() {
-  rules.forEach((className) => {
-    // transformedClassNames[className] = rules.filter(rule => rule.className === className)
-  })
+function mountCSS(styles, className) {
+  let obj = {}
+  obj[styles[0].property] = styles[0].value
+  transformedRules[className] = obj
 }
 
+function transform(rules) {
+  if (rules.length && rules[0]) {
+    let matches = []
+    let classNames = ''
 
+    const newRules = rules.filter((filteredRule) => {
+      if (filteredRule.value === rules[0].value &&
+      filteredRule.property === rules[0].property) {
+        matches.push(filteredRule)
+        classNames += filteredRule.className + ' '
+      } else {
+        return filteredRule
+      }
+    })
+
+    mountCSS(matches, classNames.trim())
+    return transform(newRules)
+  }
+}
 
 function createClass(style) {
   const className = '.mw-' + Math.random().toString(36).substring(7);
-  if (!rules[className]) {
-    rules[className] = {};
-  }
-
   Object.keys(style).forEach((property) => {
-    rules[className][property] = style[property]
+    rules.push({property: property, value: style[property], className: className})
   });
-
-  normalize();
 
   return className;
 }
@@ -51,6 +66,10 @@ createClass(input1)
 createClass(input2)
 createClass(input3)
 
-transform()
+transform(rules)
 
-console.log(rules)
+console.log(transformedRules)
+
+console.timeEnd('ended with')
+
+// console.log(rules)
